746aa391d97d0e1aa72af038e3299b10a129876f
duivvv <geoffrey.dhuyvetters@gmail.com>
2014-02-26 10:11:01 +0100

    added sql snippets

--- php/test.php
+++ php/test.php
@@ -1,11 +1,12 @@
 <?php
 
 $sql = "INSERT INTO `products` (`ok`,`test`,`ok`)
-		VALUES ()";
+		VALUES (:ok,:test,:ok)";
 $stmt = $pdo->prepare($sql);
+$stmt->bindValue(":ok",$ok);
+$stmt->bindValue(":test",$test);
 if($stmt->execute()){
 	
 }
-
 
 ?>

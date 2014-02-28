0b9966db365f00c2829eec8377d7f5c97be61323
duivvv <geoffrey.dhuyvetters@gmail.com>
2014-02-28 13:31:33 +0100

    add get snippet

--- php/test.php
+++ php/test.php
@@ -1,12 +1,17 @@
 <?php
 
-$sql = "INSERT INTO `products` (`ok`,`test`,`ok`)
-		VALUES (:ok,:test,:ok)";
-$stmt = $pdo->prepare($sql);
-$stmt->bindValue(":ok",$ok);
-$stmt->bindValue(":test",$test);
-if($stmt->execute()){
-	
+public function getProduct($id){
+    $sql = "SELECT * FROM `product` 
+    		WHERE `id` = :id";
+    $stmt = ->pdo->prepare();
+    $stmt->bindValue(":id",$id);		
+    if($stmt->execute()){
+        $product = ->fetch(PDO::FETCH_ASSOC);
+        if(!empty($product)){
+            return $product;
+        }
+    }
+    return array();
 }
 
 ?>
